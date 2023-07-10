70e4a026d91f935987abf18f6b934da3b8615768
sean-seibel <98188291+sean-seibel@users.noreply.github.com>
2023-07-09 22:05:03 -0400

    Add files via upload

---
+++ yousort/main.js
@@ -0,0 +1,460 @@
+const build = (l) => {
+    let a = [];
+    for (i = 1; i <= l; i++) {
+        a.push(i);
+    }
+    return a;
+}
+
+let _LENGTH = 32;
+let _ARR = build(_LENGTH);
+let _MAX = Math.max(..._ARR);
+let _IDLE = true;
+let _SELECTED = null;
+
+const selectBar = (bar) => {
+    if (_SELECTED) {
+        _SELECTED.classList.remove('select-bar');
+    }
+    _SELECTED = bar;
+    if (bar) {
+        bar.classList.add('select-bar');
+    }
+}
+
+const _ARRf = () => {
+    const el = document.getElementById('bar-zone');
+    const arr = new Array(el.childElementCount);
+    for (let i = 0; i < el.childElementCount; i++) {
+        arr[i] = el.children[i].getAttribute('value');
+    }
+    return arr;
+}
+const _LENGTHf = () => {
+    return document.getElementById('bar-zone').childElementCount;
+}
+
+let _needCompile = false;
+
+const compilerator = new MutationObserver(() => { /*console.log('compile');*/ _needCompile = true; refresh(false); });
+compilerator.observe(document.getElementById('text'), {
+    subtree: true,
+    childList: true,
+    characterData: true,
+})
+
+const marginSize =  (n) => {
+    if (n < 15) return 5;
+    if (n < 40) return 4;
+    if (n < 100) return 3;
+    if (n < 200) return 2;
+    if (n < 350) return 1;
+    return 0;
+}
+
+const alignBarHeight = (bar) => {
+    bar.style.height = "calc(100% * " + (parseInt(bar.getAttribute('value'))/_MAX) + ")";
+}
+
+const setBar = (bar, n) => {
+    const wasMax = bar.getAttribute('value') == _MAX;
+    bar.setAttribute('value', n);
+    if (wasMax || n > _MAX) {
+        if (n < _MAX) {
+            bar.parentNode.childNodes.forEach((e) => {
+                _MAX = Math.max(_MAX, e.getAttribute('value'));
+            });
+        } else {
+            _MAX = n;
+        }
+        bar.parentNode.childNodes.forEach((e) => {
+            alignBarHeight(e);
+        });
+    } else {
+        alignBarHeight(bar);
+    }
+}
+
+let lastLength = -1;
+const drawBars = (arr) => { // only call this once
+    const el = bars;
+    const max = Math.max(...arr);
+    const margin = marginSize(arr.length) + "px";
+    const valueInput = document.getElementById('value-input');
+    el.replaceChildren(...arr.slice(0, arr.length).map((val) => {
+        const bar = document.createElement('div');
+        bar.style.backgroundColor = "#FFF379";
+        bar.style.width = "100%";
+        bar.style.height = "calc(100% * " + (val/max) + ")";
+        bar.style.marginLeft = margin;
+        // 256 -> 1, 128 -> 2, 50 -> 3, 15 -> 4, 8 -> 5
+        // 512 -> 0
+        // bar.style.borderRadius = "";
+        bar.classList.add("rounded-sm");
+        bar.setAttribute('value', val);
+
+        bar.onmouseover = () => { if (!_SELECTED) valueInput.value = bar.getAttribute('value'); };
+        bar.onmouseout = () => { if (!_SELECTED) valueInput.value = ""; };
+        bar.onclick = () => { 
+            if (_SELECTED != bar) { 
+                valueInput.value = bar.getAttribute('value'); 
+                selectBar(bar); 
+            } else { 
+                selectBar(null); 
+            }};
+        return bar;
+    }));
+    
+    _MAX = max;
+    _SELECTED = null;
+};
+const refresh = (b) => {
+    document.getElementById("ptr").textContent = _PTR + " / " + (document.getElementById('text').childElementCount);
+    if (b) {
+        // drawBars(bars);
+    }
+};
+
+// pred : (node) -> bool
+const getChildIndex = (par, pred) => {
+    return [...par.childNodes.entries()].findIndex((val) => {
+        return pred(val[1]);
+    });
+};
+
+let _INSTRUCTIONS = [];
+let _PTR = 0;
+let _VAR_TABLE = new Map(); // string to number
+let _FLAG_TABLE = new Map(); // string to number
+
+const _SWAP = 0;
+const _SET = 1;
+const _STORE = 2;
+const _FLAG = 3;
+const _JUMP = 4;
+const _JUMPIF = 5;
+const _VAL_INPUT = 0;
+const _EXPRESSION = 1;
+const _VALUE = 2;
+const _INST_CODES = ['swap', 'set', 'store', 'flag', 'jump', 'jumpif'];
+const _SUB_CODES = ['val-input', 'expression', 'value']
+
+const executeInstruction = () => {
+    if (_needCompile) {
+        compileInstructions();
+        _needCompile = false;
+    }
+
+    if (_PTR >= _INSTRUCTIONS.length) {
+        return;
+    }
+
+    let command = _INSTRUCTIONS[_PTR];
+
+    const bar_l = bars.childNodes;
+
+    switch (command[0]) {
+        case _SWAP:
+            let i1 = computeSub(command[1], bar_l);
+            let i2 = computeSub(command[2], bar_l);
+            // let temp = _ARR[i1];
+            // _ARR[i1] = _ARR[i2];
+            // _ARR[i2] = temp;
+            let temp = bar_l[i1].getAttribute('value');
+            setBar(bar_l[i1], bar_l[i2].getAttribute('value'));
+            setBar(bar_l[i2], temp);
+            break;
+        case _SET:
+            // _ARR[
+            //     computeSub(command[1])
+            // ] = computeSub(command[2]);
+            setBar(bar_l[computeSub(command[1], bar_l)], computeSub(command[2], bar_l));
+            break;
+        case _STORE:
+            _VAR_TABLE.set(
+                command[1], // don't let them do this if its a number? bc you can't retrieve it
+                computeSub(command[2], bar_l)
+            )
+            break;
+        case _FLAG: break;
+        case _JUMP:
+            _PTR = _FLAG_TABLE.get(command[1]); // will drop you just after the flag
+            break;
+        case _JUMPIF:
+            if (computeSub(command[2], bar_l)) {
+                _PTR = _FLAG_TABLE.get(command[1]); // will drop you just after the flag
+            }
+            break;
+    }
+
+    _PTR++;
+    console.log(_ARRf());
+    console.log(_VAR_TABLE);
+    refresh(); // this might have to redraw bars if you use [set]
+};
+
+const computeSub = (sub, nodes) => {
+    switch (sub[0]) {
+        case _VAL_INPUT:
+            if (Number.isInteger(+sub[1])) {
+                return Number(sub[1]);
+            } else {
+                return _VAR_TABLE.get(sub[1]);
+            }
+        case _EXPRESSION:
+            let func = _FUNCS[sub[1]];
+            return func(computeSub(sub[2], bar_l), computeSub(sub[3], bar_l));
+        case _VALUE:
+            return nodes(computeSub(sub[1], bar_l)).getAttribute('value');
+    }
+}
+
+const compileInstructions = () => {
+    let instructions = [];
+    const text = document.getElementById("text");
+    text.childNodes.forEach((node) => {
+        if (node?.classList) {
+            const inst = compileInstruction(node);
+            //console.log(inst);
+            instructions.push(inst);    
+        }
+    });
+    _VAR_TABLE.clear();
+    _VAR_TABLE.set("LENGTH", _LENGTH);
+    _FLAG_TABLE.clear();
+    for(p = 0; p < instructions.length; p++) {
+        if (instructions[p][0] == _FLAG) {
+            _FLAG_TABLE.set(instructions[p][1], p);
+        }
+    }
+    //console.log(_FLAG_TABLE);
+    _INSTRUCTIONS = instructions;
+    setCookie('i', JSON.stringify(_INSTRUCTIONS), 365);
+}
+
+const compileInstruction = (instNode) => {
+    let inst = [];
+    inst.push(
+        _INST_CODES.findIndex((st) => st == instNode.classList.item(0))
+    );
+    // inst.push(instNode); // for highlighting (if you add this update indicies in computing somehow)
+    const accepts = childrenWithClass(instNode, 'accepts1');
+    const inputs = childrenWithClass(instNode, 'val-input');
+    switch (inst[0]) {
+        case _SWAP:
+            inst.push(compileSubcomputation(accepts[0]));
+            inst.push(compileSubcomputation(accepts[1]));
+            break;
+        case _SET:
+            inst.push(compileSubcomputation(accepts[0]));
+            inst.push(compileSubcomputation(accepts[1]));
+            break;
+        case _STORE:
+            inst.push(inputs[0].textContent);
+            inst.push(compileSubcomputation(accepts[0]));
+            break;
+        case _FLAG:
+            inst.push(inputs[0].textContent);
+            break;
+        case _JUMP:
+            inst.push(inputs[0].textContent);
+            break;
+        case _JUMPIF:
+            inst.push(inputs[0].textContent);
+            inst.push(compileSubcomputation(accepts[0]));
+            break;
+    }
+    return inst;
+};
+
+const compileSubcomputation = (accept1) => {
+    const child = accept1.firstChild;
+    if (!child) { return null; }
+    // const cl = child.classList?.item(0);
+    const cl = _SUB_CODES.findIndex((st) => st == child.classList?.item(0))
+    const operands = childrenWithClass(child, 'accepts1'); // only immediate children
+    switch (cl) {
+        case _VAL_INPUT:
+            return [_VAL_INPUT, child.textContent];
+        case _EXPRESSION:
+            return [
+                _EXPRESSION,
+                child.getElementsByClassName("op-sel")[0].value,
+                compileSubcomputation(operands[0]),
+                compileSubcomputation(operands[1])
+            ];
+        case _VALUE:
+            return [_VALUE, compileSubcomputation(operands[0])];
+    }
+    return null;
+};
+
+document.getElementById('randomize-button').onclick = () => { drawBars(randomizeARRf()); };
+
+document.getElementById('copy-button').onclick = () => {
+    navigator.clipboard.writeText(JSON.stringify(_INSTRUCTIONS)).then(
+        () => { alert('Copied!'); },
+        () => { alert('Copy failed :('); }
+    );
+};
+
+document.getElementById('paste-button').onclick = () => {
+    let pasteText = prompt('Paste here:');
+    loadFromInstructions(pasteText);
+}
+
+const fromCookie = getCookie('i');
+if (fromCookie) {
+    loadFromInstructions(fromCookie);
+    compileInstructions();
+}
+
+const highlight = (ptr) => {
+    let i = 0;
+    document.getElementById('text').childNodes.forEach((val) => {
+        if (i == ptr) {
+            val.classList?.add('highlighted');
+        } else {
+            val.classList?.remove('highlighted');
+        }
+        i++;
+    });
+};
+
+const setStopActive = (active) => {
+    if (active) {
+        document.getElementById('start-button').classList.add('disabled');
+        document.getElementById('step-button').classList.add('disabled');
+        document.getElementById('stop-button').classList.remove('disabled');
+        document.getElementById('pause-button').classList.remove('disabled');
+    } else {
+        document.getElementById('start-button').classList.remove('disabled');
+        document.getElementById('step-button').classList.remove('disabled');
+        document.getElementById('stop-button').classList.add('disabled');
+        document.getElementById('pause-button').classList.add('disabled');
+    }
+}
+
+let _RUN_ID = 0;
+
+const runWithDelay = (delay) => {
+    _IDLE = false;
+    setStopActive(true);
+    console.log('running');
+    _RUN_ID++;
+    tick(delay, _RUN_ID);
+}
+
+const tick = (delay, run_id) => {
+    if (_RUN_ID != run_id || _PTR >= _INSTRUCTIONS.length) {
+        _IDLE = true;
+        if (_PTR >= _INSTRUCTIONS.length) { _PTR = 0; /*refresh(false);*/ }
+        highlight(-1);
+        setStopActive(false);
+        refresh(false);
+        console.log('done');
+        return;
+    }
+    highlight(_PTR);
+    const err = executeInstruction();
+    if (err) {
+        alert('error');
+        return;
+    }
+    setTimeout(tick, delay, delay, run_id);
+}
+
+document.getElementById('start-button').onclick = () => runWithDelay(1000);
+document.getElementById('start-button').onmouseenter = () => highlight(_PTR);
+document.getElementById('start-button').onmouseleave = () => { if (_IDLE) highlight(-1); }
+
+const stop = (reset) => {
+    if (reset) { _PTR = 0; }
+    _RUN_ID++;
+    setStopActive(false);
+    highlight(-1);
+}
+
+document.getElementById('stop-button').onclick = () => stop(true);
+document.getElementById('pause-button').onclick = () => stop(false);
+
+document.getElementById('step-button').onmouseenter = () => highlight(_PTR);
+document.getElementById('step-button').onmouseleave = () => highlight(-1);
+document.getElementById('step-button').onclick = () => {
+    executeInstruction();
+    highlight(_PTR);
+};
+
+const setLength = (l) => {
+    if (l < 1 || l > 500) { document.getElementById('length-input').value = _LENGTHf(); return false; }
+
+    const arr = _ARRf();
+
+    if (l > arr.length) {
+        for (let i = arr.length + 1; i <= l; i++) {
+            arr.push(i);
+        }
+        console.log(arr);
+    }
+
+    drawBars(arr.slice(0,l));
+
+    document.getElementById('length-label').textContent = "Length: (" + _LENGTHf() + ")";
+    document.getElementById('length-input').value = _LENGTHf();
+    return true;
+};
+setLength(_ARR.length);
+
+document.getElementById('length-set').onclick = () => {
+    // set length, clear input, alert if unsuccessful
+    const num = parseInt(document.getElementById('length-input').value || "0");
+    if (setLength(num)) { // if (success)
+        refresh(true);
+    } else {
+        alert('Length invalid: ' + num);
+    }
+};
+
+document.getElementById('length-remake').onclick = () => {
+    drawBars(build(_LENGTHf()));
+    refresh(true);
+}
+
+document.getElementById('length-import').onclick = () => {
+    const inputStr = prompt('Enter a separated list of positive integers');
+    if (!inputStr) { return; }
+    const numList = inputStr.split(/[^0123456789]+/).filter((s) => {
+        return s;
+    });
+    if (numList.length == 0) { alert('No numbers found!'); return; }
+    const values = numList.map((s) => {
+        return parseInt(s);
+    });
+    // console.log(values);
+    drawBars(values);
+    refresh(true);
+}
+
+document.getElementById('length-copy').onclick = () => {
+    navigator.clipboard.writeText(JSON.stringify(_ARRf())).then(
+        () => { alert('Copied values!'); },
+        () => { alert('Copy failed :('); }
+    );
+}
+
+document.getElementById('value-set').onclick = () => {
+    const valIn = document.getElementById('value-input');
+    if (!_SELECTED) { valIn.value = ""; return; }
+    // set length, clear input, alert if unsuccessful
+    const num = parseInt(valIn.value);
+    console.log(num);
+    if (num != NaN) { // if (success)
+        setBar(_SELECTED, num);
+    } else {
+        alert('Value invalid: ' + num);
+    }
+    valIn.value = _SELECTED.getAttribute('value');
+};
+
+drawBars(_ARR);
+refresh(true);
---
+++ yousort/setup.js
@@ -0,0 +1,306 @@
+const bars = document.getElementById("bar-zone");
+
+// drags
+
+const dragPrim = dragula([document.getElementById("source"), document.getElementById("text")], {
+    copy: (el, source) => {
+      return source === document.getElementById("source");
+    },
+    accepts: (el, target) => {
+      return target.classList.contains("accepts");
+    },
+    invalid: (el, target) => {
+        return !_IDLE || !el.classList.contains("prim");
+    },
+    removeOnSpill: true,
+  })
+
+const _source = document.getElementById("source");
+const dragSub = dragula([_source], {
+    isContainer: (c) => {
+        return c.classList.contains('accepts1') && !_source.contains(c) && !c.parentElement.classList.contains('gu-transit');
+    },
+    copy: (el, source) => {
+        return source === document.getElementById("source");
+      },
+    accepts: (el, target) => {
+        const child = getFirstNonTransitSub(target); // ig this works
+        // console.log(child);
+        return target.classList.contains("accepts1") && !child;
+            // ^ gu-transit is used for the placement shadow
+            // basically forbid acceptance if there's an object in there that's not my shadow
+    },
+    invalid: (el, target) => {
+        return !_IDLE || !el.classList.contains("sub");
+    },
+    removeOnSpill: true,
+    // direction: 'horizontal',
+});
+dragPrim.on('drop', (el, target, source, sibling) => {
+    refresh(false);
+});
+
+// on drop into accepts1: (drop)
+//      replace editable node with dropped node
+
+// on over: (over)
+//      make editable node invisible (set font size to 0)
+
+// on not over: (out)
+//      make editable node visible (remove font 0)
+
+// on block being removed from accepts1 (drop & remove)
+//      create new editable node, and append it to the place that lost the original node
+
+// container = latest container shadow has been in
+
+
+dragSub.on('drop', (el, target, source, sibling) => {
+    target.replaceChildren(el);
+
+    if (source.classList.contains('accepts1')) {
+        source.replaceChildren(makeEditableTextNode());
+    }
+});
+
+dragSub.on('remove', (el, container, source) => {
+    if (source.classList.contains('accepts1')) {
+        source.replaceChildren(makeEditableTextNode());
+    }
+});
+
+dragSub.on('over', (el, container, source) => {
+    if (container.classList?.contains('accepts1')) {
+        container.childNodes.forEach((val) => {
+            if (val.classList?.contains('val-input')) {
+                val.classList.add('no-font');
+            }
+        });
+    }
+});
+
+dragSub.on('out', (el, container, source) => {
+    if (container.classList?.contains('accepts1')) {
+        container.childNodes.forEach((val) => {
+            val.classList?.remove('no-font');
+        });
+    }
+});
+
+const getFirstNonTransitSub = (par) => {
+    return [...par.childNodes].find((val) => {
+        return (val).classList?.contains('sub') && !(val).classList?.contains('gu-transit');
+    });
+};
+
+let barFrom = 0;
+const dragBars = dragula([bars], {
+    invalid: () => { return !_IDLE; },
+    direction: 'horizontal'
+}).on('drag', (el) => {
+    barFrom = getChildIndex(bars, (n) => { return n == el });
+}).on('drop', (el) => {
+    const barTo = getChildIndex(bars, (n) => { return n == el });
+    const temp = _ARR[barFrom];
+    _ARR.splice(barFrom, 1);
+    _ARR.splice(barTo, 0, temp);
+    //console.log(_ARR); // ^ this reflects dragged bars in _ARR
+});
+
+// populating components
+
+// 0 = False, !0 = True
+const _FUNCS = {
+    "+": (a, b) => a + b, 
+    "–": (a, b) => a - b, 
+    "*": (a, b) => a * b,  
+    "/": (a, b) => Math.floor(a / b),  
+    "%": (a, b) => a % b, 
+    "=": (a, b) => a == b ? 1 : 0, 
+    ">": (a, b) => a > b ? 1 : 0, 
+    "<": (a, b) => a < b ? 1 : 0, 
+    "≥": (a, b) => a >= b ? 1 : 0, 
+    "≤": (a, b) => a <= b ? 1 : 0, 
+    "r": (a, b) => Math.floor(Math.random() * (b-a)) + a,
+};
+
+const ops = document.getElementById("op-sel")
+if (ops) {
+    Object.keys(_FUNCS).forEach((op) => {
+        const option = document.createElement("option");
+        option.text = op;
+        option.value = op;
+        ops.add(option);
+    });
+}
+
+// yeah you can still paste stuff in whaaaatever
+const valueInput = document.getElementsByClassName('val-input');
+[...valueInput].forEach((inp) => {
+    inp.addEventListener('keydown', (evt) => {
+        if (evt.key === "Enter") {
+            evt.preventDefault();
+        }
+    });
+});
+
+const makeEditableTextNode = (text) => {
+    const editableTextNode = document.createElement('pre');
+    editableTextNode.classList.add('val-input');
+    editableTextNode.classList.add('text-center');
+    editableTextNode.setAttribute('contenteditable', true);
+    if (text) { editableTextNode.textContent = text; }
+    return editableTextNode
+}
+
+document.querySelectorAll(".accepts1").forEach((el) => {
+    el.appendChild(
+        makeEditableTextNode()
+    );
+});
+
+// on events
+
+document.onkeydown = (e) => {
+    e = e || window.event;
+    switch (e.key) {
+        case "c":
+            compileInstructions();
+            _needCompile = false;
+            _PTR = 0;
+            refresh();
+            break;
+        case "r":
+            randomizeARR();
+            break;
+        case " ":
+            highlight(_PTR);
+            executeInstruction();
+            break;
+        case "p":
+            dragSub.containers.forEach((val) => {
+                console.log(val.parentElement);
+            })
+            break;
+        case "Escape": selectBar(null); break;
+        case "h": highlight(-1); break;
+        case "0": highlight(0); break;
+        case "1": highlight(1); break;
+        case "2": highlight(2); break;
+        case "3": highlight(3); break;
+        case "4": highlight(4); break;
+        case "5": highlight(5); break;
+        case ",": setStopActive(false); break;
+        case ".": setStopActive(true); break;
+        default: break;
+    }
+};
+
+document.onkeyup = (e) => {
+    e = e || window.event;
+    switch (e.key) {
+        case " ": highlight(-1); break;
+        default: break;
+    }
+}
+
+const fischerYatesShuffle = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+const randomizeARRf = () => { // and refresh
+    const arr = _ARRf();
+    fischerYatesShuffle(arr);
+    return arr;
+}
+
+const childrenWithClass = (node, cl) => {
+    return [...node.children].filter((child) => {
+        return child.classList.contains(cl);
+    })
+};
+
+const swapBlock = childrenWithClass(_source, 'swap')[0];
+const setBlock = childrenWithClass(_source, 'set')[0];
+const storeBlock = childrenWithClass(_source, 'store')[0];
+const flagBlock = childrenWithClass(_source, 'flag')[0];
+const jumpBlock = childrenWithClass(_source, 'jump')[0];
+const jumpifBlock = childrenWithClass(_source, 'jumpif')[0];
+const expressionBlock = childrenWithClass(_source, 'expression')[0];
+const valueBlock = childrenWithClass(_source, 'value')[0];
+
+const loadFromInstructions = (st) => {
+    const text = document.getElementById('text');
+    
+    const instsFromPaste = JSON.parse(st);
+
+    if (!instsFromPaste) { return; }
+
+    text.replaceChildren(...instsFromPaste.map((inst) => {
+        let node, accepts, valInputs;
+        switch (inst[0]) {
+            case _SWAP:
+                node = swapBlock.cloneNode(true);
+                accepts = childrenWithClass(node, 'accepts1');
+                accepts[0].replaceChildren(loadSub(inst[1]));
+                accepts[1].replaceChildren(loadSub(inst[2]));
+                return node;
+            case _SET:
+                node = setBlock.cloneNode(true);
+                accepts = childrenWithClass(node, 'accepts1');
+                accepts[0].replaceChildren(loadSub(inst[1]));
+                accepts[1].replaceChildren(loadSub(inst[2]));
+                return node;
+            case _STORE:
+                node = storeBlock.cloneNode(true);
+                accepts = childrenWithClass(node, 'accepts1');
+                valInputs = childrenWithClass(node, 'val-input');
+                valInputs[0].textContent = inst[1];
+                accepts[0].replaceChildren(loadSub(inst[2]));
+                return node;
+            case _FLAG:
+                node = flagBlock.cloneNode(true);
+                valInputs = childrenWithClass(node, 'val-input');
+                valInputs[0].textContent = inst[1];
+                return node;
+            case _JUMP:
+                node = jumpBlock.cloneNode(true);
+                valInputs = childrenWithClass(node, 'val-input');
+                valInputs[0].textContent = inst[1];
+                return node;
+            case _JUMPIF:
+                node = jumpifBlock.cloneNode(true);
+                accepts = childrenWithClass(node, 'accepts1');
+                valInputs = childrenWithClass(node, 'val-input');
+                valInputs[0].textContent = inst[1];
+                accepts[0].replaceChildren(loadSub(inst[2]));
+                return node;        
+        }
+    }));
+
+    // make all accept1s draggable ( we did this by using our IQ )
+    // dragSub.containers.push(...text.querySelectorAll('.accepts1'));
+};
+
+const loadSub = (sub) => {
+    switch (sub[0]) {
+        case _VAL_INPUT:
+            return makeEditableTextNode(sub[1]);
+        case _EXPRESSION:
+            let enode = expressionBlock.cloneNode(true);
+            let eaccepts = childrenWithClass(enode, 'accepts1');
+            eaccepts[0].replaceChildren(loadSub(sub[2]));
+            eaccepts[1].replaceChildren(loadSub(sub[3]));
+            childrenWithClass(enode, 'op-sel')[0].value = sub[1];
+            return enode;
+        case _VALUE:
+            let vnode = valueBlock.cloneNode(true);
+            let vaccepts = childrenWithClass(vnode, 'accepts1');
+            vaccepts[0].replaceChildren(loadSub(sub[1]));
+            return vnode;
+    }
+};
